677c2ad018a194aec847b9ed7bd42c865d7dfaeb
FelipeOropeza <felipe.oropeza@etec.sp.gov.br>
2022-09-30 16:12:04 -0300

    Teste

--- Aula6/conexao.php
+++ Aula6/conexao.php
@@ -1,19 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Conexão</title>
-</head>
-<body>
-    <?php
+<?php
     $servidor = 'localhost';
     $usuario = 'root';
     $senha = 'kiraFE22022006';
     $banco = 'BD_A';
 
     $cn = new PDO("mysql:host=$servidor;dbname=$banco",$usuario,$senha);
-    ?>
-</body>
-</html>
+?>
